8ffcd9949c3da681ec905d9838ba5aafb5126d20
quangpn24 <19522100@gm.uit.edu.vn>
2023-03-15 16:54:25 +0700

    3rd

---
+++ 206.ReverseLinkedList/206.ReverseLinkedList.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	utils "daily_leetcode_challenge"
+	"fmt"
+)
+
+type ListNode = utils.ListNode
+
+// solution 1
+func reverseList(head *ListNode) *ListNode {
+	var pre *ListNode
+	current := head
+	for current != nil {
+		next := current.Next
+		current.Next = pre
+		pre = current
+		current = next
+	}
+	return pre
+}
+
+// solution2
+// reverse(list) = first + reverser(remaining)
+func reverseListByRecursive(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	next := head.Next
+	newHead := reverseListByRecursive(next)
+	next.Next = head
+	head.Next = nil
+	return newHead
+}
+
+func main() {
+	list := &ListNode{1, &ListNode{2, &ListNode{4, nil}}}
+	res := reverseList(list)
+	fmt.Println(res.Val)
+}
---
+++ 21.MergeTwoSortedLists/21.MergeTwoSortedLists.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	utils "daily_leetcode_challenge"
+	"fmt"
+)
+
+type ListNode = utils.ListNode
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	resList := &ListNode{}
+	tmpList := resList
+	for list1 != nil || list2 != nil {
+		if list1 == nil {
+			tmpList.Next = list2
+			break
+		}
+		if list2 == nil {
+			tmpList.Next = list1
+			break
+		}
+		if list1.Val > list2.Val {
+			tmpList.Next = &ListNode{list2.Val, nil}
+			list2 = list2.Next //increase step
+			tmpList = tmpList.Next
+		} else if list1.Val < list2.Val {
+			tmpList.Next = &ListNode{list1.Val, nil}
+			list1 = list1.Next // increase step
+			tmpList = tmpList.Next
+		} else {
+			tmpList.Next = &ListNode{list1.Val, &ListNode{list2.Val, nil}}
+			list1 = list1.Next // increase step
+			list2 = list2.Next // increase step
+			tmpList = tmpList.Next.Next
+		}
+	}
+	return resList.Next
+}
+func main() {
+	//list1 := &ListNode{1, &ListNode{2, &ListNode{4, nil}}}
+	//list2 := &ListNode{1, &ListNode{3, &ListNode{4, nil}}}
+	list1 := &ListNode{1, nil}
+	list2 := &ListNode{2, nil}
+	resList := mergeTwoLists(list1, list2)
+	fmt.Println(resList.Val)
+}
