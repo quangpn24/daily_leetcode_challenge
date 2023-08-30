fa516d756416e28a998bc54f1adf110797ca05fb
quangpn24 <19522100@gm.uit.edu.vn>
2023-08-30 10:43:41 +0700

    Some problem

--- 102.BinaryTreeLevelOrderTraversal/102.BinaryTreeLevelOrderTraversal.go
+++ 102.BinaryTreeLevelOrderTraversal/102.BinaryTreeLevelOrderTraversal.go
@@ -1,27 +1,39 @@
 package main
 
 import (
-	"daily_leetcode_challenge/utils"
-	"fmt"
+    "daily_leetcode_challenge/utils"
+    "fmt"
 )
 
 type TreeNode = utils.TreeNode
 
-func enqueue(queue []*TreeNode, element *TreeNode) []*TreeNode {
-	queue = append(queue, element) // Simply append to enqueue.
-	fmt.Println("Enqueued:", element)
-	return queue
-}
-
-func dequeue(queue []*TreeNode) (*TreeNode, []*TreeNode) {
-	element := queue[0] // The first element is the one to be dequeued.
-	if len(queue) == 1 {
-		return element, []*TreeNode{}
-
-	}
-	return element, queue[1:] // Slice off the element once it is dequeued.
-}
-
 func levelOrder(root *TreeNode) [][]int {
-
+    if root == nil {
+        return nil
+    }
+    queue := []*TreeNode{root}
+    res := [][]int{}
+    for len(queue) > 0 {
+        valueInLevel := []int{}
+        for _, node := range queue {
+            if node.Left != nil {
+                queue = append(queue, node.Left)
+            }
+            if node.Right != nil {
+                queue = append(queue, node.Right)
+            }
+            valueInLevel = append(valueInLevel, node.Val)
+            queue = queue[1:]
+        }
+        res = append(res, valueInLevel)
+    }
+    return res
+}
+func main() {
+    root := &TreeNode{
+        Val:   3,
+        Left:  &TreeNode{Val: 9},
+        Right: &TreeNode{20, &TreeNode{Val: 15}, &TreeNode{Val: 7}},
+    }
+    fmt.Println(levelOrder(root))
 }
